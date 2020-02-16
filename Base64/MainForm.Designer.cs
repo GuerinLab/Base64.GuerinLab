97866933c6c7c9c3fbd1306aeec1d13862448b27
Carl <carl@guerinso.com>
2020-02-15 23:55:45 -0500

    Final

---
+++ Base64/MainForm.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Base64
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LabelFormTitle = new System.Windows.Forms.Label();
+            this.InputTextbox = new System.Windows.Forms.TextBox();
+            this.OutputTextbox = new System.Windows.Forms.TextBox();
+            this.DecodeBt = new System.Windows.Forms.Button();
+            this.EncodeBt = new System.Windows.Forms.Button();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.InputLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(129)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.LabelFormTitle);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 30);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Base64.Properties.Resources.IconForm;
+            this.pictureBox4.Location = new System.Drawing.Point(6, 3);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(26, 24);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = global::Base64.Properties.Resources.GithubBT;
+            this.pictureBox3.Location = new System.Drawing.Point(505, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(26, 24);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::Base64.Properties.Resources.MinBT;
+            this.pictureBox2.Location = new System.Drawing.Point(537, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(26, 24);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::Base64.Properties.Resources.CloseBT;
+            this.pictureBox1.Location = new System.Drawing.Point(569, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(26, 24);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // LabelFormTitle
+            // 
+            this.LabelFormTitle.AutoSize = true;
+            this.LabelFormTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelFormTitle.ForeColor = System.Drawing.Color.White;
+            this.LabelFormTitle.Location = new System.Drawing.Point(33, 5);
+            this.LabelFormTitle.Name = "LabelFormTitle";
+            this.LabelFormTitle.Size = new System.Drawing.Size(38, 20);
+            this.LabelFormTitle.TabIndex = 0;
+            this.LabelFormTitle.Text = "Title";
+            // 
+            // InputTextbox
+            // 
+            this.InputTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InputTextbox.Location = new System.Drawing.Point(6, 80);
+            this.InputTextbox.Multiline = true;
+            this.InputTextbox.Name = "InputTextbox";
+            this.InputTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.InputTextbox.Size = new System.Drawing.Size(589, 130);
+            this.InputTextbox.TabIndex = 1;
+            // 
+            // OutputTextbox
+            // 
+            this.OutputTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputTextbox.Location = new System.Drawing.Point(6, 245);
+            this.OutputTextbox.Multiline = true;
+            this.OutputTextbox.Name = "OutputTextbox";
+            this.OutputTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputTextbox.Size = new System.Drawing.Size(589, 143);
+            this.OutputTextbox.TabIndex = 2;
+            // 
+            // DecodeBt
+            // 
+            this.DecodeBt.Location = new System.Drawing.Point(520, 216);
+            this.DecodeBt.Name = "DecodeBt";
+            this.DecodeBt.Size = new System.Drawing.Size(75, 23);
+            this.DecodeBt.TabIndex = 3;
+            this.DecodeBt.Text = "Decode";
+            this.DecodeBt.UseVisualStyleBackColor = true;
+            this.DecodeBt.Click += new System.EventHandler(this.DecodeBt_Click);
+            // 
+            // EncodeBt
+            // 
+            this.EncodeBt.Location = new System.Drawing.Point(439, 216);
+            this.EncodeBt.Name = "EncodeBt";
+            this.EncodeBt.Size = new System.Drawing.Size(75, 23);
+            this.EncodeBt.TabIndex = 4;
+            this.EncodeBt.Text = "Encode";
+            this.EncodeBt.UseVisualStyleBackColor = true;
+            this.EncodeBt.Click += new System.EventHandler(this.EncodeBt_Click);
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputLabel.Location = new System.Drawing.Point(3, 226);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(81, 16);
+            this.OutputLabel.TabIndex = 5;
+            this.OutputLabel.Text = "Output string";
+            // 
+            // InputLabel
+            // 
+            this.InputLabel.AutoSize = true;
+            this.InputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputLabel.Location = new System.Drawing.Point(3, 61);
+            this.InputLabel.Name = "InputLabel";
+            this.InputLabel.Size = new System.Drawing.Size(71, 16);
+            this.InputLabel.TabIndex = 6;
+            this.InputLabel.Text = "Input string";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Base64.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.InputLabel);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.EncodeBt);
+            this.Controls.Add(this.DecodeBt);
+            this.Controls.Add(this.OutputTextbox);
+            this.Controls.Add(this.InputTextbox);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label LabelFormTitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox InputTextbox;
+        private System.Windows.Forms.TextBox OutputTextbox;
+        private System.Windows.Forms.Button DecodeBt;
+        private System.Windows.Forms.Button EncodeBt;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Label InputLabel;
+    }
+}
+
---
+++ Base64/MainForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Base64
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            LabelFormTitle.Text = "Base64";
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(new Pen(Color.FromArgb(71, 129, 255), 3),
+                         this.DisplayRectangle);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/GuerinLab");
+        }
+
+        public static byte[] Base64Decoding(char[] data)
+        {
+            int length, length2, length3;
+            int blockCount;
+            int paddingCount = 0;
+
+            length = data.Length;
+            blockCount = length / 4;
+            length2 = blockCount * 3;
+
+            for (int x = 0; x < 2; x++)
+            {
+                if (data[length - x - 1] == '=')
+                    paddingCount++;
+            }
+
+            byte[] buffer = new byte[length];
+            byte[] buffer2 = new byte[length2];
+
+            for (int x = 0; x < length; x++)
+            {
+                buffer[x] = CharToSixBit(data[x]);
+            }
+
+            byte b, b1, b2, b3;
+            byte temp1, temp2, temp3, temp4;
+
+            for (int x = 0; x < blockCount; x++)
+            {
+                temp1 = buffer[x * 4];
+                temp2 = buffer[x * 4 + 1];
+                temp3 = buffer[x * 4 + 2];
+                temp4 = buffer[x * 4 + 3];
+
+                b = (byte)(temp1 << 2);
+                b1 = (byte)((temp2 & 48) >> 4);
+                b1 += b;
+
+                b = (byte)((temp2 & 15) << 4);
+                b2 = (byte)((temp3 & 60) >> 2);
+                b2 += b;
+
+                b = (byte)((temp3 & 3) << 6);
+                b3 = temp4;
+                b3 += b;
+
+                buffer2[x * 3] = b1;
+                buffer2[x * 3 + 1] = b2;
+                buffer2[x * 3 + 2] = b3;
+            }
+
+            length3 = length2 - paddingCount;
+            byte[] result = new byte[length3];
+
+            for (int x = 0; x < length3; x++)
+            {
+                result[x] = buffer2[x];
+            }
+
+            return result;
+        }
+
+        private static byte CharToSixBit(char c)
+        {
+            char[] lookupTable = new char[64] {
+        'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+        'O','P','Q','R','S','T','U','V','W','X','Y', 'Z',
+        'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+        'o','p','q','r','s','t','u','v','w','x','y','z',
+        '0','1','2','3','4','5','6','7','8','9','+','/'
+    };
+
+            if (c == '=')
+            {
+                return 0;
+            }
+            else
+            {
+                for (int x = 0; x < 64; x++)
+                {
+                    if (lookupTable[x] == c)
+                        return (byte)x;
+                }
+
+                return 0;
+            }
+        }
+
+        public static char[] Base64Encoding(byte[] data)
+        {
+            int length, length2;
+            int blockCount;
+            int paddingCount;
+
+            length = data.Length;
+
+            if ((length % 3) == 0)
+            {
+                paddingCount = 0;
+                blockCount = length / 3;
+            }
+            else
+            {
+                paddingCount = 3 - (length % 3);
+                blockCount = (length + paddingCount) / 3;
+            }
+
+            length2 = length + paddingCount;
+
+            byte[] source2;
+            source2 = new byte[length2];
+
+            for (int x = 0; x < length2; x++)
+            {
+                if (x < length)
+                {
+                    source2[x] = data[x];
+                }
+                else
+                {
+                    source2[x] = 0;
+                }
+            }
+
+            byte b1, b2, b3;
+            byte temp, temp1, temp2, temp3, temp4;
+            byte[] buffer = new byte[blockCount * 4];
+            char[] result = new char[blockCount * 4];
+
+            for (int x = 0; x < blockCount; x++)
+            {
+                b1 = source2[x * 3];
+                b2 = source2[x * 3 + 1];
+                b3 = source2[x * 3 + 2];
+
+                temp1 = (byte)((b1 & 252) >> 2);
+
+                temp = (byte)((b1 & 3) << 4);
+                temp2 = (byte)((b2 & 240) >> 4);
+                temp2 += temp;
+
+                temp = (byte)((b2 & 15) << 2);
+                temp3 = (byte)((b3 & 192) >> 6);
+                temp3 += temp;
+
+                temp4 = (byte)(b3 & 63);
+
+                buffer[x * 4] = temp1;
+                buffer[x * 4 + 1] = temp2;
+                buffer[x * 4 + 2] = temp3;
+                buffer[x * 4 + 3] = temp4;
+
+            }
+
+            for (int x = 0; x < blockCount * 4; x++)
+            {
+                result[x] = SixBitToChar(buffer[x]);
+            }
+
+            switch (paddingCount)
+            {
+                case 0:
+                    break;
+                case 1:
+                    result[blockCount * 4 - 1] = '=';
+                    break;
+                case 2:
+                    result[blockCount * 4 - 1] = '=';
+                    result[blockCount * 4 - 2] = '=';
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+
+        private static char SixBitToChar(byte b)
+        {
+            char[] lookupTable = new char[64] {
+        'A','B','C','D','E','F','G','H','I','J','K','L','M',
+        'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+        'a','b','c','d','e','f','g','h','i','j','k','l','m',
+        'n','o','p','q','r','s','t','u','v','w','x','y','z',
+        '0','1','2','3','4','5','6','7','8','9','+','/'
+    };
+
+            if ((b >= 0) && (b <= 63))
+            {
+                return lookupTable[(int)b];
+            }
+            else
+            {
+                return ' ';
+            }
+        }
+
+        private void EncodeBt_Click(object sender, EventArgs e)
+        {
+            if(InputTextbox.Text != string.Empty)
+            {
+                byte[] data = System.Text.Encoding.Default.GetBytes(InputTextbox.Text);
+                char[] value = Base64Encoding(data);
+                string str = new string(value);
+                OutputTextbox.Text = str;
+            }
+        }
+
+        private void DecodeBt_Click(object sender, EventArgs e)
+        {
+            if(InputTextbox.Text != string.Empty)
+            {
+                string data = InputTextbox.Text;
+                byte[] value = Base64Decoding(data.ToCharArray());
+                OutputTextbox.Text = Encoding.UTF8.GetString(value, 0, value.Length);
+            }
+        }
+    }
+}
